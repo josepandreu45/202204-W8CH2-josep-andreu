8f0937b227594164826bfc83cce68b826bf9f8da
josep andreu <josepa45@gmail.com>
2022-05-26 21:52:35 +0200

    Created createRobot function and reorganized files

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
-import Robot from "./types/types";
+import Robot from "./clases/Robot";
+import { IRobot, RobotType } from "./types/types";
 
-const jacobo = new Robot("Cleaner");
-const jacob = [];
-jacob.push(jacobo);
+const createRobot = (type: RobotType): IRobot => {
+  const newRobot = new Robot(type);
+  return newRobot;
+};
+
+const robot = createRobot("Waiter");
+console.log(robot.name);
+robot.work();
+robot.reset();
+console.log(robot.name);
