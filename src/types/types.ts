789ce829d5a2ef0852bb6195e180d3c062121736
josep andreu <josepa45@gmail.com>
2022-05-26 21:37:37 +0200

    Created methods random name and work

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,23 +2,60 @@ type RobotType = "Cleaner" | "Waiter" | "Developer";
 
 interface IRobot {
   name: string;
-  type: RobotType;
   battery: number;
+  type: RobotType;
   work: () => void;
   reset: () => void;
 }
 
 class Robot implements IRobot {
   battery;
-  constructor(
-    public name: string,
-    public type: RobotType,
-    private message: string
-  ) {
+  name;
+
+  constructor(public type: RobotType) {
+    this.battery = 100;
+    this.name = this.generateName();
+  }
+
+  reset() {
+    this.name = this.generateName();
     this.battery = 100;
   }
   work() {
-    console.log(this.message);
+    let message: string;
+
+    switch (this.type) {
+      case "Cleaner":
+        message = "Lararara I clean";
+        break;
+
+      case "Developer":
+        message = "Do you feel like a mini of fuet?";
+        break;
+
+      case "Waiter":
+        message = "JavaScript is cool - I develop with JavaScript -> I'm cool";
+
+      default:
+        message = "";
+    }
+    console.log(message);
   }
-  reset() {}
+  generateName(): string {
+    let name: string = "";
+
+    const chars: string = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+
+    for (let i = 0; i <= 3; i++) {
+      if (i < 3) {
+        name += chars.charAt(Math.floor(Math.random() * chars.length));
+      } else {
+        name += Math.floor(Math.random() * (99 - 10) + 10);
+      }
+    }
+
+    return name;
+  }
 }
+
+export default Robot;
